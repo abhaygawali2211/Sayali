dd09eb716e751961ff502c5776ec6d737d615453
abhaygawali2211 <abhaygawali2211@gmail.com>
2022-11-14 16:32:51 +0530

    this is the first mistek

---
+++ SayaliP/src/com/velocity/Sayali.java
@@ -0,0 +1,12 @@
+package com.velocity;
+
+public class Sayali {
+
+	public static void main(String[] args) {
+		
+		System.out.println(" Hellow  sayli mam");
+		// TODO Auto-generated method stub
+
+	}
+
+}
